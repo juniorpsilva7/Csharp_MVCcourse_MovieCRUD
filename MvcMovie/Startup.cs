0e41d89ce57a5e5a79963e3d60952263195247f6
Junior Silva <juniorpsilva7@gmail.com>
2016-10-17 09:44:46 -0200

    Commit creation

---
+++ MvcMovie/Startup.cs
@@ -0,0 +1,15 @@
+﻿using MvcMovie;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace MvcMovie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
